90a59fb85326ec2194fe39bde48d7679c9b49617
Minji Kim <sberryyogurt@gmail.com>
2016-11-27 18:10:53 +0900

    20161127

--- Circular Array Loop/Circular Array Loop.py
+++ Circular Array Loop/Circular Array Loop.py
@@ -1,4 +1,10 @@
-# You are given an array of positive and negative integers. If a number n at an index is positive, then move forward n steps. Conversely, if it's negative (-n), move backward n steps. Assume the first element of the array is forward next to the last element, and the last element is backward next to the first element. Determine if there is a loop in this array. A loop starts and ends at a particular index with more than 1 element along the loop. The loop must be "forward" or "backward'.
+# 457. Circular Array Loop (https://leetcode.com/problems/circular-array-loop/)
+# You are given an array of positive and negative integers.
+# If a number n at an index is positive, then move forward n steps.
+# Conversely, if it's negative (-n), move backward n steps.
+# Assume the first element of the array is forward next to the last element, and the last element is backward next to the first element.
+# Determine if there is a loop in this array. A loop starts and ends at a particular index with more than 1 element along the loop.
+# The loop must be "forward" or "backward'.
 #
 # Example 1: Given the array [2, -1, 1, 2, 2], there is a loop, from index 0 -> 2 -> 3 -> 0.
 #
