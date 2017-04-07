b75e4875d9e8286aad4049614c2078be484e4588
Minji Kim <sberryyogurt@gmail.com>
2017-04-08 01:25:32 +0900

    20170408

--- Largest Number/Largest_Number.py
+++ Largest Number/Largest_Number.py
@@ -17,7 +17,7 @@ class Solution:
         result = list()
         for i in range(0, len(nums)):
             for j in range(i, len(nums)):
-                if int(str(nums[i])+str(nums[j])) > int(str(nums[j])+str(nums[i])):
+                if int(str(nums[i]) + str(nums[j])) > int(str(nums[j]) + str(nums[i])):
                     nums[i], nums[j] = nums[j], nums[i]
 
         for num in nums:
@@ -26,9 +26,7 @@ class Solution:
         return str(int("".join(result[::-1])))
 
 
-
 if __name__ == "__main__":
     sol = Solution()
-    # print(sol.largestNumber([1, 2, 30]))
     print(sol.largestNumber([1, 2, 30]))
     print(sol.largestNumber([93, 51, 65, 84, 91, 78, 99, 71, 97, 9]))
