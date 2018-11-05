31cce5fc8566cb1cba08d6d0c9bdec55a9aae4f0
iamminji <minji.dev@gmail.com>
2018-11-05 20:18:49 +0900

    20181105 [Easy] string 문제

---
+++ 709.To-Lower-Case/ToLowerCase.go
@@ -0,0 +1,10 @@
+// 709. To Lower Case
+// https://leetcode.com/problems/to-lower-case/description/
+
+package main
+
+import "strings"
+
+func toLowerCase(str string) string {
+	return strings.ToLower(str)
+}
