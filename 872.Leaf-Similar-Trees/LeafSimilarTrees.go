0abe29774eca428ff9bd2034b0300318d4373512
iamminji <minji.dev@gmail.com>
2018-12-04 07:51:51 +0900

    20181204 [Easy]

---
+++ 872.Leaf-Similar-Trees/LeafSimilarTrees.go
@@ -0,0 +1,48 @@
+// 872. Leaf-Similar Trees
+// https://leetcode.com/problems/leaf-similar-trees/submissions/
+
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func recursive(node *TreeNode, ans *[]int){
+
+	if node.Left == nil && node.Right == nil {
+		*ans = append(*ans, node.Val)
+		return
+	}
+	if node.Left != nil {
+		recursive(node.Left, ans)
+	}
+	if node.Right != nil {
+		recursive(node.Right, ans)
+	}
+	return
+}
+
+func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+	a := make([]int, 0)
+	b := make([]int, 0)
+
+	recursive(root1, &a)
+	recursive(root2, &b)
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for idx, v := range a {
+		if b[idx] != v {
+			return false
+		}
+	}
+	return true
+}
---
+++ 872.Leaf-Similar-Trees/solutions.md
@@ -0,0 +1,9 @@
+#### 문제 풀이
+
+[Leaf-Similar Trees](https://leetcode.com/problems/leaf-similar-trees/submissions/)
+
+
+난이도는 Easy다.
+
+트리 순회하면서 자식 노드가 동일한지에 대한 여부를 리턴하면 된다.
+Go 로 했고 slice 포인터 넘기고 그 값들을 비교했다.
