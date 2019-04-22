4f737b13f10516a86958ec7dadc8d8f965d7d53f
iamminji <minji.dev@gmail.com>
2019-04-22 22:56:32 +0900

    제곱근이 아니라 거듭제곱임

--- 🙂easy/0231.Power-of-Two/power_of_two.py
+++ 🙂easy/0231.Power-of-Two/power_of_two.py
@@ -3,12 +3,12 @@
 
 
 class Solution:
-    # 2의 제곱근이 맞는지 리턴하는 문제다.
+    # power of 2 인지 리턴하는 문제다.
     def isPowerOfTwo(self, n: int) -> bool:
         if n < 0:
             return False
         # bit count 를 한다.
-        # 2의 제곱근은 1이 하나밖에 없다.
+        # 어떤 수가 power of 2면 1이 하나밖에 없다.
         return bin(n).count("1") == 1
 
     def isPowerOfTwo2(self, n: int) -> bool:
