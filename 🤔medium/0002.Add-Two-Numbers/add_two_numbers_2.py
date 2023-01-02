e0874d6e2f033d6f5963a3e6686b2e101319f309
iamminji <iamminji@linecorp.com>
2023-01-02 21:19:54 +0900

    [20230102] solve medium problem

---
+++ 🤔medium/0002.Add-Two-Numbers/add_two_numbers_2.py
@@ -0,0 +1,37 @@
+# 2. Add Two Numbers
+# https://leetcode.com/problems/add-two-numbers/
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        c = 0
+        result = ListNode(0)
+        dummy = result
+        while l1 is not None or l2 is not None:
+            lv1 = l1.val if l1 is not None else 0
+            lv2 = l2.val if l2 is not None else 0
+
+            r = (lv1 + lv2 + c) % 10
+            c = (lv1 + lv2 + c) // 10
+
+            dummy.next = ListNode(r)
+
+            if l1 is not None:
+                l1 = l1.next
+            if l2 is not None:
+                l2 = l2.next
+
+            dummy = dummy.next
+
+        if c > 0:
+            dummy.next = ListNode(c)
+
+        return result.next
